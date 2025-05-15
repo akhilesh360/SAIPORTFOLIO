09ebaf1fcef6198568755cdcdbbf5cbc8bf34e0d
Sai Akhilesh Veldi <70189275+akhilesh360@users.noreply.github.com>
2025-05-15 11:03:13 -0500

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,7 +8,7 @@ import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
-import aiDocIntelligence from "../../Assets/Projects/aiDocIntelligence.png"; // Import the new project image
+import aiDocIntelligence from "../../Assets/Projects/aiDocIntelligence.png"; 
 
 function Projects() {
   return (
